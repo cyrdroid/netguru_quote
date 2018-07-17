24d5239c6dbdb5b2943cfbce404c44df2405b6e0
cyrdroid <cyrdroid@gmail.com>
2018-07-18 01:23:29 +0200

    First Commit

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,25 @@
+class QuotesController < ApplicationController
+  def index
+    @quotes = Quote.all.order("created_at DESC")
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to "http://localhost:3000/quotes"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:body)
+  end
+end
